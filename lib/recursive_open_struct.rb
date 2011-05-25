41eed3ad6eaf836a30742ff726acc4e6ac9e30f8
Offirmo <offirmo.net@gmail.com>
2011-05-25 17:45:14 +0200

    added a debug function

--- lib/recursive_open_struct.rb
+++ lib/recursive_open_struct.rb
@@ -1,6 +1,7 @@
 require 'ostruct'
 
 class RecursiveOpenStruct < OpenStruct
+  
   def new_ostruct_member(name)
     name = name.to_sym
     unless self.respond_to?(name)
@@ -15,4 +16,34 @@ class RecursiveOpenStruct < OpenStruct
     end
     name
   end
+  
+  def debug_inspect(indent_level = 0, recursion_limit = 12)
+    display_recursive_open_struct(@table, indent_level, recursion_limit)
+  end
+  
+  def display_recursive_open_struct(ostrct_or_hash, indent_level, recursion_limit)
+    
+    if recursion_limit <= 0 then
+      # protection against recursive structure (like in the tests)
+      puts '  '*indent_level + '(recursion limit reached)'
+    else
+      #puts ostrct_or_hash.inspect
+      if ostrct_or_hash.is_a?(RecursiveOpenStruct) then
+        ostrct_or_hash = ostrct_or_hash.marshal_dump
+      end
+      
+      data_indent = ostrct_or_hash.max {|a,b| a[0].to_s.length <=> b[0].to_s.length}[0].length
+      ostrct_or_hash.each do |key, value|
+        if (value.is_a?(RecursiveOpenStruct) || value.is_a?(Hash)) then
+          puts '  '*indent_level + key.to_s + '.'
+          display_recursive_open_struct(value, indent_level + 1, recursion_limit - 1)
+        else
+          puts '  '*indent_level + key.to_s + ' '*(data_indent - key.to_s.length) + '= ' + value.inspect
+        end
+      end
+    end
+    
+    true
+  end
+
 end
--- spec/recursive_open_struct_spec.rb
+++ spec/recursive_open_struct_spec.rb
@@ -2,40 +2,41 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'recursive_open_struct'
 
 describe RecursiveOpenStruct do
+  
   describe "behavior it inherits from OpenStruct" do
-
+    
     it "can represent arbitrary data objects" do
       ros = RecursiveOpenStruct.new
       ros.blah = "John Smith"
       ros.blah.should == "John Smith"
     end
-
+    
     it "can be created from a hash" do
       h = { :asdf => 'John Smith' }
       ros = RecursiveOpenStruct.new(h)
       ros.asdf.should == "John Smith"
     end
-
+    
     it "can modify an existing key" do
       h = { :blah => 'John Smith' }
       ros = RecursiveOpenStruct.new(h)
       ros.blah = "George Washington"
       ros.blah.should == "George Washington"
     end
-
+    
     describe "handling of arbitrary attributes" do
       before(:each) do
         @ros = RecursiveOpenStruct.new
         @ros.blah = "John Smith"
       end
-
+      
       describe "#respond?" do
         it { @ros.should respond_to :blah }
         it { @ros.should respond_to :blah= }
         it { @ros.should_not respond_to :asdf }
         it { @ros.should_not respond_to :asdf= }
       end # describe #respond?
-
+      
       describe "#methods" do
         it { @ros.methods.should include :blah }
         it { @ros.methods.should include :blah= }
@@ -50,15 +51,15 @@ describe RecursiveOpenStruct do
       h = { :blah => { :another => 'value' } }
       @ros = RecursiveOpenStruct.new(h)
     end
-
+    
     it "returns accessed hashes as RecursiveOpenStructs instead of hashes" do
       @ros.blah.another.should == 'value'
     end
-
+    
     it "uses #key_as_a_hash to return key as a Hash" do
       @ros.blah_as_a_hash.should == { :another => 'value' }
     end
-
+    
     describe "handling loops in the origin Hashes" do
       before(:each) do
         h1 = { :a => 'a'}
@@ -66,7 +67,7 @@ describe RecursiveOpenStruct do
         h1[:h2] = h2
         @ros = RecursiveOpenStruct.new(h2)
       end
-
+      
       it { @ros.h1.a.should == 'a' }
       it { @ros.h1.h2.a.should == 'b' }
       it { @ros.h1.h2.h1.a.should == 'a' }
@@ -75,4 +76,19 @@ describe RecursiveOpenStruct do
       it { @ros.h1.should_not == @ros.h1.h2 }
     end # describe handling loops in the origin Hashes
   end # recursive behavior
+  
+  describe "additionnel features" do
+    
+    before(:each) do
+      h1 = { :a => 'a'}
+      h2 = { :a => 'b', :h1 => h1 }
+      h1[:h2] = h2
+      @ros = RecursiveOpenStruct.new(h2)
+    end
+    
+    it "should have a simple way of display" do
+      @ros.debug_inspect
+    end
+  end # additionnel features
+  
 end # describe RecursiveOpenStruct
