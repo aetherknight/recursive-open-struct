edbb52487de235930b99ed068921f5da492b4216
Offirmo <offirmo.net@gmail.com>
2011-05-31 15:56:36 +0200

    slight display improvement of the debug function

--- lib/recursive_open_struct.rb
+++ lib/recursive_open_struct.rb
@@ -32,13 +32,20 @@ class RecursiveOpenStruct < OpenStruct
         ostrct_or_hash = ostrct_or_hash.marshal_dump
       end
       
-      data_indent = ostrct_or_hash.max {|a,b| a[0].to_s.length <=> b[0].to_s.length}[0].length
+      # We'll display the key values like this :    key =  value
+      # to align display, we look for the maximum key length of the data that will be displayed
+      # (everything except hashes)
+      data_indent = ostrct_or_hash
+        .reject { |k, v| v.is_a?(RecursiveOpenStruct) || v.is_a?(Hash) }
+          .max {|a,b| a[0].to_s.length <=> b[0].to_s.length}[0].length
+      # puts "max length = #{data_indent}"
+      
       ostrct_or_hash.each do |key, value|
         if (value.is_a?(RecursiveOpenStruct) || value.is_a?(Hash)) then
           puts '  '*indent_level + key.to_s + '.'
           display_recursive_open_struct(value, indent_level + 1, recursion_limit - 1)
         else
-          puts '  '*indent_level + key.to_s + ' '*(data_indent - key.to_s.length) + '= ' + value.inspect
+          puts '  '*indent_level + key.to_s + ' '*(data_indent - key.to_s.length) + ' = ' + value.inspect
         end
       end
     end
