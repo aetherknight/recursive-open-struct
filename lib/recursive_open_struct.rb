b52bacc16faa457dd5341fd9690d2a231508c263
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2012-10-23 19:12:08 -0700

    Fixing whitespace

--- lib/recursive_open_struct.rb
+++ lib/recursive_open_struct.rb
@@ -1,7 +1,7 @@
 require 'ostruct'
 
 class RecursiveOpenStruct < OpenStruct
-  
+
   def new_ostruct_member(name)
     name = name.to_sym
     unless self.respond_to?(name)
@@ -22,13 +22,13 @@ class RecursiveOpenStruct < OpenStruct
     end
     name
   end
-  
+
   def debug_inspect(io = STDOUT, indent_level = 0, recursion_limit = 12)
     display_recursive_open_struct(io, @table, indent_level, recursion_limit)
   end
-  
+
   def display_recursive_open_struct(io, ostrct_or_hash, indent_level, recursion_limit)
-    
+
     if recursion_limit <= 0 then
       # protection against recursive structure (like in the tests)
       io.puts '  '*indent_level + '(recursion limit reached)'
@@ -37,7 +37,7 @@ class RecursiveOpenStruct < OpenStruct
       if ostrct_or_hash.is_a?(RecursiveOpenStruct) then
         ostrct_or_hash = ostrct_or_hash.marshal_dump
       end
-      
+
       # We'll display the key values like this :    key =  value
       # to align display, we look for the maximum key length of the data that will be displayed
       # (everything except hashes)
@@ -45,7 +45,7 @@ class RecursiveOpenStruct < OpenStruct
         .reject { |k, v| v.is_a?(RecursiveOpenStruct) || v.is_a?(Hash) }
           .max {|a,b| a[0].to_s.length <=> b[0].to_s.length}[0].length
       # puts "max length = #{data_indent}"
-      
+
       ostrct_or_hash.each do |key, value|
         if (value.is_a?(RecursiveOpenStruct) || value.is_a?(Hash)) then
           io.puts '  '*indent_level + key.to_s + '.'
@@ -55,7 +55,7 @@ class RecursiveOpenStruct < OpenStruct
         end
       end
     end
-    
+
     true
   end
 
