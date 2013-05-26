3e5102f995325635d72fd4389bc3dd0a63b858aa
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2013-05-26 16:03:29 -0700

    Fixing spec:coverage

--- Gemfile
+++ Gemfile
@@ -1,2 +1,10 @@
 source 'http://rubygems.org'
 gemspec
+
+group :development do
+  if RUBY_VERSION =~ /^1\.8/
+    gem 'rcov'
+  else
+    gem 'simplecov'
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,15 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  SimpleCov.start
+end
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+
 end
