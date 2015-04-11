93a4cd99c16bc304ba5060e007c2b25c423577ff
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2015-04-11 13:10:48 -0700

    Moving recursion into its own file.
    It could probably be broken up into several sub-describe blocks better,
    but that is a task for another day.

--- spec/recursive_open_struct/recursion_spec.rb
+++ spec/recursive_open_struct/recursion_spec.rb
@@ -1,46 +1,46 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require_relative '../spec_helper'
 require 'recursive_open_struct'
 
 describe RecursiveOpenStruct do
-  describe "improvements on OpenStruct" do
-    it "can be converted back to a hash" do
-      blank_obj = Object.new
-      h = {:asdf => 'John Smith', :foo => [{:bar => blank_obj}, {:baz => nil}]}
-      ros = RecursiveOpenStruct.new(h)
-      ros.to_h.should == h
-      ros.to_hash.should == h
-    end
-  end
 
   describe "recursive behavior" do
     let(:h) { { :blah => { :another => 'value' } } }
     subject { RecursiveOpenStruct.new(h) }
 
+    it "can convert the entire hash tree back into a hash" do
+      blank_obj = Object.new
+      h = {:asdf => 'John Smith', :foo => [{:bar => blank_obj}, {:baz => nil}]}
+      ros = RecursiveOpenStruct.new(h)
+
+      expect(ros.to_h).to eq h
+      expect(ros.to_hash).to eq h
+    end
+
     it "returns accessed hashes as RecursiveOpenStructs instead of hashes" do
-      subject.blah.another.should == 'value'
+      expect(subject.blah.another).to eq 'value'
     end
 
     it "handles subscript notation the same way as dotted notation" do
-      subject.blah.another.should == subject[:blah].another
+      expect(subject.blah.another).to eq subject[:blah].another
     end
 
     it "uses #key_as_a_hash to return key as a Hash" do
-      subject.blah_as_a_hash.should == { :another => 'value' }
+      expect(subject.blah_as_a_hash).to eq({ :another => 'value' })
     end
 
-    describe "handling loops in the origin Hashes" do
+    describe "handling loops in the original Hashes" do
       let(:h1) { { :a => 'a'} }
       let(:h2) { { :a => 'b', :h1 => h1 } }
       before(:each) { h1[:h2] = h2 }
 
       subject { RecursiveOpenStruct.new(h2) }
 
-      it { subject.h1.a.should == 'a' }
-      it { subject.h1.h2.a.should == 'b' }
-      it { subject.h1.h2.h1.a.should == 'a' }
-      it { subject.h1.h2.h1.h2.a.should == 'b' }
-      it { subject.h1.should == subject.h1.h2.h1 }
-      it { subject.h1.should_not == subject.h1.h2 }
+      it { expect(subject.h1.a).to eq 'a' }
+      it { expect(subject.h1.h2.a).to eq 'b' }
+      it { expect(subject.h1.h2.h1.a).to eq 'a' }
+      it { expect(subject.h1.h2.h1.h2.a).to eq 'b' }
+      it { expect(subject.h1).to eq subject.h1.h2.h1 }
+      it { expect(subject.h1).to_not eq subject.h1.h2 }
     end # describe handling loops in the origin Hashes
 
     it "can modify a key of a sub-element" do
@@ -51,7 +51,8 @@ describe RecursiveOpenStruct do
       }
       ros = RecursiveOpenStruct.new(h)
       ros.blah.blargh = "Janet"
-      ros.blah.blargh.should == "Janet"
+
+      expect(ros.blah.blargh).to eq "Janet"
     end
 
     context "after a sub-element has been modified" do
@@ -68,39 +69,40 @@ describe RecursiveOpenStruct do
 
       describe ".to_h" do
         it "returns a hash tree that contains those modifications" do
-          subject.to_h.should == updated_hash
+          expect(subject.to_h).to eq updated_hash
         end
 
         specify "modifying the returned hash tree does not modify the ROS" do
           subject.to_h[:blah][:blargh] = "Dr Scott"
 
-          subject.blah.blargh.should == "Janet"
+          expect(subject.blah.blargh).to eq "Janet"
         end
       end
 
       it "does not mutate the original hash tree passed to the constructor" do
-        hash[:blah][:blargh].should == 'Brad'
+        expect(hash[:blah][:blargh]).to eq 'Brad'
       end
 
       it "limits the deep-copy to the initial hash tree" do
         subject.some_array[0] = 4
 
-        hash[:some_array][0].should == 4
+        expect(hash[:some_array][0]).to eq 4
       end
 
       describe "#dup" do
         let(:duped_subject) { subject.dup }
 
         it "preserves sub-element modifications" do
-          duped_subject.blah.blargh.should == subject.blah.blargh
+          expect(duped_subject.blah.blargh).to eq subject.blah.blargh
         end
 
         it "allows the copy's sub-elements to be modified independently from the original's" do
-          subject.blah.blargh.should == "Janet"
+          expect(subject.blah.blargh).to eq "Janet"
+
           duped_subject.blah.blargh = "Dr. Scott"
 
-          duped_subject.blah.blargh.should == "Dr. Scott"
-          subject.blah.blargh.should == "Janet"
+          expect(subject.blah.blargh).to eq "Janet"
+          expect(duped_subject.blah.blargh).to eq "Dr. Scott"
         end
       end
     end
@@ -120,11 +122,11 @@ describe RecursiveOpenStruct do
         before(:each) { subject.blah = new_blah }
 
         it "returns the modified value instead of the memoized one" do
-          subject.blah.something_new.should == "C"
+          expect(subject.blah.something_new).to eq "C"
         end
 
         specify "the old value no longer exists" do
-          subject.blah.a.should be_nil
+          expect(subject.blah.a).to be_nil
         end
       end
 
@@ -134,7 +136,7 @@ describe RecursiveOpenStruct do
         it "returns the modified value instead of the memoized one" do
           new_blah = [4, 5, 6]
           subject.blah = new_blah
-          subject.blah.should == new_blah
+          expect(subject.blah).to eq new_blah
         end
       end
     end
@@ -146,11 +148,11 @@ describe RecursiveOpenStruct do
       context "when recursing over arrays is enabled" do
         subject { RecursiveOpenStruct.new(h, :recurse_over_arrays => true) }
 
-        it { subject.blah.length.should == 3 }
-        it { subject.blah[0].foo.should == '1' }
-        it { subject.blah[1].foo.should == '2' }
-        it { subject.blah_as_a_hash.should == blah_list }
-        it { subject.blah[2].should == 'baz' }
+        it { expect(subject.blah.length).to eq 3 }
+        it { expect(subject.blah[0].foo).to eq '1' }
+        it { expect(subject.blah[1].foo).to eq '2' }
+        it { expect(subject.blah_as_a_hash).to eq blah_list }
+        it { expect(subject.blah[2]).to eq 'baz' }
 
         context "when an inner value changes" do
           let(:updated_blah_list) { [ { :foo => '1' }, { :foo => 'Dr Scott' }, 'baz' ] }
@@ -159,41 +161,41 @@ describe RecursiveOpenStruct do
           before(:each) { subject.blah[1].foo = "Dr Scott" }
 
           it "Retains changes across Array lookups" do
-            subject.blah[1].foo.should == "Dr Scott"
+            expect(subject.blah[1].foo).to eq "Dr Scott"
           end
 
           it "propagates the changes through to .to_h across Array lookups" do
-            subject.to_h.should == {
+            expect(subject.to_h).to eq({
               :blah => [ { :foo => '1' }, { :foo => "Dr Scott" }, 'baz' ]
-            }
+            })
           end
 
           it "deep-copies hashes within Arrays" do
             subject.to_h[:blah][1][:foo] = "Rocky"
 
-            subject.blah[1].foo.should == "Dr Scott"
+            expect(subject.blah[1].foo).to eq "Dr Scott"
           end
 
           it "does not mutate the input hash passed to the constructor" do
-            h[:blah][1][:foo].should == '2'
+            expect(h[:blah][1][:foo]).to eq '2'
           end
 
           it "the deep copy recurses over Arrays as well" do
-            h[:blah][1][:foo].should == '2'
+            expect(h[:blah][1][:foo]).to eq '2'
           end
 
           describe "#dup" do
             let(:duped_subject) { subject.dup }
 
             it "preserves sub-element modifications" do
-              duped_subject.blah[1].foo.should == subject.blah[1].foo
+              expect(duped_subject.blah[1].foo).to eq subject.blah[1].foo
             end
 
             it "allows the copy's sub-elements to be modified independently from the original's" do
               duped_subject.blah[1].foo = "Rocky"
 
-              duped_subject.blah[1].foo.should == "Rocky"
-              subject.blah[1].foo.should == "Dr Scott"
+              expect(duped_subject.blah[1].foo).to eq "Rocky"
+              expect(subject.blah[1].foo).to eq "Dr Scott"
             end
           end
         end
@@ -202,10 +204,10 @@ describe RecursiveOpenStruct do
           let(:deep_hash) { { :foo => { :blah => blah_list } } }
           subject { RecursiveOpenStruct.new(deep_hash, :recurse_over_arrays => true) }
 
-          it { subject.foo.blah.length.should == 3 }
+          it { expect(subject.foo.blah.length).to eq 3 }
           it "Retains changes across Array lookups" do
             subject.foo.blah[1].foo = "Dr Scott"
-            subject.foo.blah[1].foo.should == "Dr Scott"
+            expect(subject.foo.blah[1].foo).to eq "Dr Scott"
           end
 
         end
@@ -214,11 +216,12 @@ describe RecursiveOpenStruct do
           let(:haah) { { :blah => [ blah_list ] } }
           subject { RecursiveOpenStruct.new(haah, :recurse_over_arrays => true) }
 
-          it { subject.blah.length.should == 1 }
-          it { subject.blah[0].length.should == 3 }
+          it { expect(subject.blah.length).to eq 1 }
+          it { expect(subject.blah[0].length).to eq 3 }
           it "Retains changes across Array lookups" do
             subject.blah[0][1].foo = "Dr Scott"
-            subject.blah[0][1].foo.should == "Dr Scott"
+
+            expect(subject.blah[0][1].foo).to eq "Dr Scott"
           end
 
         end
@@ -228,11 +231,11 @@ describe RecursiveOpenStruct do
       context "when recursing over arrays is disabled" do
         subject { RecursiveOpenStruct.new(h) }
 
-        it { subject.blah.length.should == 3 }
-        it { subject.blah[0].should == { :foo => '1' } }
-        it { subject.blah[0][:foo].should == '1' }
+        it { expect(subject.blah.length).to eq 3 }
+        it { expect(subject.blah[0]).to eq({ :foo => '1' }) }
+        it { expect(subject.blah[0][:foo]).to eq '1' }
       end # when recursing over arrays is disabled
 
     end # recursing over arrays
   end # recursive behavior
-end # describe RecursiveOpenStruct
+end
